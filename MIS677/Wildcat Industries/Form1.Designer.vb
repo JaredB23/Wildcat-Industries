52deb4ad20bdb39cb504d1aab3b68a26331ed20a
JaredB23 <36746720+JaredB23@users.noreply.github.com>
2018-04-10 15:37:29 -0500

    Add files via upload

---
+++ MIS677/Wildcat Industries/Form1.Designer.vb
@@ -0,0 +1,440 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class recruiterForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.delete = New System.Windows.Forms.Button()
+        Me.edit = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.createReport = New System.Windows.Forms.Button()
+        Me.recruiterName = New System.Windows.Forms.Label()
+        Me.logo = New System.Windows.Forms.PictureBox()
+        Me.firstName = New System.Windows.Forms.Label()
+        Me.school = New System.Windows.Forms.Label()
+        Me.expectedGrad = New System.Windows.Forms.Label()
+        Me.phoneNumber = New System.Windows.Forms.Label()
+        Me.lastName = New System.Windows.Forms.Label()
+        Me.major = New System.Windows.Forms.Label()
+        Me.eventAttended = New System.Windows.Forms.Label()
+        Me.email = New System.Windows.Forms.Label()
+        Me.search = New System.Windows.Forms.Label()
+        Me.university = New System.Windows.Forms.Label()
+        Me.eventName = New System.Windows.Forms.Label()
+        Me.comments = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(34, 46)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(179, 44)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(160, 24)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(347, 44)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(160, 24)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(36, 98)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(473, 467)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(582, 122)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(582, 221)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(172, 22)
+        Me.TextBox4.TabIndex = 6
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(582, 269)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox5.TabIndex = 7
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(822, 269)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox6.TabIndex = 11
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(822, 172)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox8.TabIndex = 9
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(822, 122)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox9.TabIndex = 8
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(582, 380)
+        Me.TextBox10.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox10.Multiline = True
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(400, 43)
+        Me.TextBox10.TabIndex = 12
+        '
+        'delete
+        '
+        Me.delete.Location = New System.Drawing.Point(961, 524)
+        Me.delete.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.delete.Name = "delete"
+        Me.delete.Size = New System.Drawing.Size(100, 28)
+        Me.delete.TabIndex = 13
+        Me.delete.Text = "Delete"
+        Me.delete.UseVisualStyleBackColor = True
+        '
+        'edit
+        '
+        Me.edit.Location = New System.Drawing.Point(1071, 524)
+        Me.edit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.edit.Name = "edit"
+        Me.edit.Size = New System.Drawing.Size(100, 28)
+        Me.edit.TabIndex = 14
+        Me.edit.Text = "Edit"
+        Me.edit.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1180, 524)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 28)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'createReport
+        '
+        Me.createReport.Location = New System.Drawing.Point(608, 524)
+        Me.createReport.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.createReport.Name = "createReport"
+        Me.createReport.Size = New System.Drawing.Size(189, 28)
+        Me.createReport.TabIndex = 16
+        Me.createReport.Text = "Create Applicant Report"
+        Me.createReport.UseVisualStyleBackColor = True
+        '
+        'recruiterName
+        '
+        Me.recruiterName.AutoSize = True
+        Me.recruiterName.Location = New System.Drawing.Point(973, 4)
+        Me.recruiterName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.recruiterName.Name = "recruiterName"
+        Me.recruiterName.Size = New System.Drawing.Size(127, 17)
+        Me.recruiterName.TabIndex = 17
+        Me.recruiterName.Text = "**Recruiter Name**"
+        '
+        'logo
+        '
+        Me.logo.Location = New System.Drawing.Point(1108, 4)
+        Me.logo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.logo.Name = "logo"
+        Me.logo.Size = New System.Drawing.Size(133, 62)
+        Me.logo.TabIndex = 18
+        Me.logo.TabStop = False
+        '
+        'firstName
+        '
+        Me.firstName.AutoSize = True
+        Me.firstName.Location = New System.Drawing.Point(578, 98)
+        Me.firstName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.firstName.Name = "firstName"
+        Me.firstName.Size = New System.Drawing.Size(76, 17)
+        Me.firstName.TabIndex = 19
+        Me.firstName.Text = "First Name"
+        '
+        'school
+        '
+        Me.school.AutoSize = True
+        Me.school.Location = New System.Drawing.Point(578, 155)
+        Me.school.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.school.Name = "school"
+        Me.school.Size = New System.Drawing.Size(51, 17)
+        Me.school.TabIndex = 20
+        Me.school.Text = "School"
+        '
+        'expectedGrad
+        '
+        Me.expectedGrad.AutoSize = True
+        Me.expectedGrad.Location = New System.Drawing.Point(578, 204)
+        Me.expectedGrad.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.expectedGrad.Name = "expectedGrad"
+        Me.expectedGrad.Size = New System.Drawing.Size(175, 17)
+        Me.expectedGrad.TabIndex = 21
+        Me.expectedGrad.Text = "Expected Graduation Date"
+        '
+        'phoneNumber
+        '
+        Me.phoneNumber.AutoSize = True
+        Me.phoneNumber.Location = New System.Drawing.Point(578, 253)
+        Me.phoneNumber.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.phoneNumber.Name = "phoneNumber"
+        Me.phoneNumber.Size = New System.Drawing.Size(49, 17)
+        Me.phoneNumber.TabIndex = 22
+        Me.phoneNumber.Text = "Phone"
+        '
+        'lastName
+        '
+        Me.lastName.AutoSize = True
+        Me.lastName.Location = New System.Drawing.Point(818, 98)
+        Me.lastName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lastName.Name = "lastName"
+        Me.lastName.Size = New System.Drawing.Size(76, 17)
+        Me.lastName.TabIndex = 23
+        Me.lastName.Text = "Last Name"
+        '
+        'major
+        '
+        Me.major.AutoSize = True
+        Me.major.Location = New System.Drawing.Point(818, 155)
+        Me.major.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.major.Name = "major"
+        Me.major.Size = New System.Drawing.Size(43, 17)
+        Me.major.TabIndex = 24
+        Me.major.Text = "Major"
+        '
+        'eventAttended
+        '
+        Me.eventAttended.AutoSize = True
+        Me.eventAttended.Location = New System.Drawing.Point(818, 204)
+        Me.eventAttended.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.eventAttended.Name = "eventAttended"
+        Me.eventAttended.Size = New System.Drawing.Size(105, 17)
+        Me.eventAttended.TabIndex = 25
+        Me.eventAttended.Text = "Event Attended"
+        '
+        'email
+        '
+        Me.email.AutoSize = True
+        Me.email.Location = New System.Drawing.Point(818, 250)
+        Me.email.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.email.Name = "email"
+        Me.email.Size = New System.Drawing.Size(42, 17)
+        Me.email.TabIndex = 26
+        Me.email.Text = "Email"
+        '
+        'search
+        '
+        Me.search.AutoSize = True
+        Me.search.Location = New System.Drawing.Point(31, 25)
+        Me.search.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.search.Name = "search"
+        Me.search.Size = New System.Drawing.Size(53, 17)
+        Me.search.TabIndex = 27
+        Me.search.Text = "Search"
+        '
+        'university
+        '
+        Me.university.AutoSize = True
+        Me.university.Location = New System.Drawing.Point(176, 23)
+        Me.university.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.university.Name = "university"
+        Me.university.Size = New System.Drawing.Size(70, 17)
+        Me.university.TabIndex = 28
+        Me.university.Text = "University"
+        '
+        'eventName
+        '
+        Me.eventName.AutoSize = True
+        Me.eventName.Location = New System.Drawing.Point(344, 25)
+        Me.eventName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.eventName.Name = "eventName"
+        Me.eventName.Size = New System.Drawing.Size(85, 17)
+        Me.eventName.TabIndex = 29
+        Me.eventName.Text = "Event Name"
+        '
+        'comments
+        '
+        Me.comments.AutoSize = True
+        Me.comments.Location = New System.Drawing.Point(579, 361)
+        Me.comments.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.comments.Name = "comments"
+        Me.comments.Size = New System.Drawing.Size(74, 17)
+        Me.comments.TabIndex = 30
+        Me.comments.Text = "Comments"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(822, 221)
+        Me.ComboBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(160, 24)
+        Me.ComboBox3.TabIndex = 31
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(583, 170)
+        Me.ComboBox4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(160, 24)
+        Me.ComboBox4.TabIndex = 32
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.logo)
+        Me.Panel1.Controls.Add(Me.recruiterName)
+        Me.Panel1.Controls.Add(Me.ComboBox2)
+        Me.Panel1.Controls.Add(Me.eventName)
+        Me.Panel1.Controls.Add(Me.search)
+        Me.Panel1.Controls.Add(Me.university)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1278, 93)
+        Me.Panel1.TabIndex = 33
+        '
+        'recruiterForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(1285, 570)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.comments)
+        Me.Controls.Add(Me.email)
+        Me.Controls.Add(Me.eventAttended)
+        Me.Controls.Add(Me.major)
+        Me.Controls.Add(Me.lastName)
+        Me.Controls.Add(Me.phoneNumber)
+        Me.Controls.Add(Me.expectedGrad)
+        Me.Controls.Add(Me.school)
+        Me.Controls.Add(Me.firstName)
+        Me.Controls.Add(Me.createReport)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.edit)
+        Me.Controls.Add(Me.delete)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "recruiterForm"
+        Me.Text = "Recruiter Form"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents delete As Button
+    Friend WithEvents edit As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents createReport As Button
+    Friend WithEvents recruiterName As Label
+    Friend WithEvents logo As PictureBox
+    Friend WithEvents firstName As Label
+    Friend WithEvents school As Label
+    Friend WithEvents expectedGrad As Label
+    Friend WithEvents phoneNumber As Label
+    Friend WithEvents lastName As Label
+    Friend WithEvents major As Label
+    Friend WithEvents eventAttended As Label
+    Friend WithEvents email As Label
+    Friend WithEvents search As Label
+    Friend WithEvents university As Label
+    Friend WithEvents eventName As Label
+    Friend WithEvents comments As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ MIS677/Wildcat Industries/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class recruiterForm
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles search.Click
+
+    End Sub
+
+    Private Sub name_Click(sender As Object, e As EventArgs) Handles firstName.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles expectedGrad.Click
+
+    End Sub
+End Class
